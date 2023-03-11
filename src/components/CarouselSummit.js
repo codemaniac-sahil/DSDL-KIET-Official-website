4b150d6a63788bc3f75f69b058f9d1bef64378e7
Yash-Var <yashvarshney7011@gmail.com>
2023-03-11 22:59:47 +0530

    added

--- src/components/CarouselSummit.js
+++ src/components/CarouselSummit.js
@@ -1,7 +1,12 @@
 import React from "react";
+import IndividualIntervalsExample from "./IndividualIntervalsExample";
 // import "bootstrap";
 const CarouselSummit = () => {
-  return <div></div>;
+  return (
+    <div>
+      <IndividualIntervalsExample />
+    </div>
+  );
 };
 
 export default CarouselSummit;
